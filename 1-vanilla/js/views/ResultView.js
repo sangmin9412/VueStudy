8dd1e50f4e91ba1f0a3a6edf7dc69a13bed9bbd0
SangMin <tkdals9048@naver.com>
2020-08-31 16:14:59 +0900

    1-vanilla end

---
+++ 1-vanilla/js/views/ResultView.js
@@ -0,0 +1,37 @@
+import View from './View.js';
+
+const tag = '[ResultView]';
+
+const ResultView = Object.create(View);
+
+ResultView.messages = {
+    NO_RESULT: '검색 결과가 없습니다'
+}
+
+ResultView.setup = function(el) {
+    this.init(el)
+}
+
+ResultView.render = function(data = []) {
+    console.log(tag, 'render()', data)
+    this.el.innerHTML = data.length ? this.getSearchResultHtml(data) : this.messages.NO_RESULT
+    this.show();
+}
+
+ResultView.getSearchResultHtml = function(data) {
+    return data.reduce((html, item) => {
+        html += this.getSearchItemHtml(item)
+        return html
+    }, '<ul>') + '</ul>'
+}
+
+ResultView.getSearchItemHtml = function(item) {
+    return `
+        <li>
+            <img src="${item.image}" alt="${item.name}">
+            <p>${item.name}</p>
+        </li>
+    `
+}
+
+export default ResultView
---
+++ 1-vanilla/js/views/TabView.js
@@ -0,0 +1,32 @@
+import View from './View.js'
+
+const tag = '[TabView]'
+
+const TabView = Object.create(View);
+
+TabView.setup = function(el) {
+    this.init(el)
+    this.bindEvents()
+    return this
+}
+
+TabView.setActiveTab = function(tabName) {
+    Array.from(this.el.querySelectorAll('li')).forEach(li => {
+        li.className = li.innerHTML === tabName ? 'active' : ''
+    })
+    this.show()
+}
+
+TabView.bindEvents = function() {
+    // this.el.addEventListener('click', e => this.onClickTab(e))
+    Array.from(this.el.querySelectorAll('li')).forEach(li => {
+        li.addEventListener('click', e => this.onClick(li.innerHTML))
+    })
+}
+
+TabView.onClick = function(tabName) {
+    this.setActiveTab(tabName)
+    this.emit('@change', { tabName })
+}
+
+export default TabView
